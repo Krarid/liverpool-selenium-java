d3be3785bea25f9c514372e4722f3b836cf3c01c
Javier Meléndez <jmz.krarid@outlook.com>
2024-01-31 15:34:24 -0600

    Feat: BaseTest
    BaseTest class contains the driver manager

---
+++ Liverpool/src/test/java/utils/BaseTest.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void ConfigureDriver()
+	{
+		driver = WebDriverManager.edgedriver().create();
+				
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
