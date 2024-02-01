c6caf1f453990a3f45a7e3743b7904ee6ee33b24
Javier Meléndez <jmz.krarid@outlook.com>
2024-02-01 15:03:10 -0600

    Feat: Place the URL as a global variable

--- Liverpool/src/test/java/utils/BaseTest.java
+++ Liverpool/src/test/java/utils/BaseTest.java
@@ -1,25 +1,39 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import pageObjects.HomePage;
 
-public class BaseTest {
+public class BaseTest extends WebUtils {
 	
 	public WebDriver driver;
+	public HomePage home;
+	public Properties property;
 	
 	@BeforeClass
-	public void ConfigureDriver()
+	public void ConfigureDriver() throws IOException
 	{
+		property = new Properties();
+		FileInputStream propertyFile = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
+		
+		property.load(propertyFile);
+		
 		driver = WebDriverManager.edgedriver().create();
 				
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.manage().window().maximize();
 		
-		driver.get("https://www.liverpool.com.mx/tienda/home");
+		driver.get(property.getProperty("url"));
+		
+		home = new HomePage(driver);
 	}
 	
 	@AfterClass
