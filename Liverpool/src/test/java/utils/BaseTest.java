ea74bf20c9fd0aea93a5d1a6c8fb300a820dfcb2
Javier Meléndez <jmz.krarid@outlook.com>
2024-02-02 11:21:24 -0600

    Feat: Dataprovider method belongs to the BaseTest class

--- Liverpool/src/test/java/utils/BaseTest.java
+++ Liverpool/src/test/java/utils/BaseTest.java
@@ -2,12 +2,17 @@ package utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObjects.HomePage;
@@ -34,6 +39,26 @@ public class BaseTest extends WebUtils {
 		driver.get(property.getProperty("url"));
 		
 		home = new HomePage(driver);
+	}
+	
+	@DataProvider
+	public Object[][] getData(Method m) throws IOException
+	{	
+		List<HashMap<String, String>> data = getJsonData(System.getProperty("user.dir") + "\\src\\test\\java\\data\\" + m.getName() + ".json");
+		
+		Object[][] objData = new Object[data.size()][1];
+		
+		for(int i = 0; i < data.size(); i++) {
+			objData[i][0] = data.get(i);
+		}
+		
+		return objData;
+	}
+	
+	@AfterMethod
+	public void returnToHome() throws IOException
+	{
+		driver.navigate().to(property.getProperty("url"));
 	}
 	
 	@AfterClass
